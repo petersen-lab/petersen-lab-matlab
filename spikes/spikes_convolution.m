734b5b49f0e598b58946f594b91459286d470664
dervinism <martynas.dervinis@gmail.com>
2023-06-15 19:44:21 +0200

    added concatenateCells, fixed spikes_convolution

--- spikes/spikes_convolution.m
+++ spikes/spikes_convolution.m
@@ -1,7 +1,7 @@
 function [spikes_presentation,time_bins] = spikes_convolution(spikes, stepsize, convolution_points)
-% Gaussian convolution of the spikes raster into continue rates 
+% Gaussian convolution of the spikes raster into continuous rates 
 % Inputs
-%   spikes              % Spikes struct
+%   spikes              % Spikes struct with numcells and times fields
 %   stepsize            % step size of continues traces
 %   convolution_points  % points of gaussian convolution (gausswin)
 %
@@ -16,8 +16,10 @@ if nargin < 3
     % Setting default convolution points (steps)
     convolution_points = 50;
 end
-if isfield(spikes,'spindices')
+if isfield(spikes,'times')
     spikes.spindices = generateSpinDices(spikes.times);
+else
+    error('Spikes structure is missing spike times.');
 end
 
 % Generating continues representation of the raster actvity
@@ -25,8 +27,9 @@ time_bins = 0:stepsize:ceil(max(spikes.spindices(:,1)));
 spikes_presentation = zeros(spikes.numcells,numel(time_bins));
 
 for i = 1:spikes.numcells
-    idx = round(spikes.times{i}/stepsize);
-    spikes_presentation(i,idx) = 1;
+    idx = round(spikes.times{i}/stepsize); % Assign bin indices to spikes
+    [spkCounts,idx] = groupcounts(idx); % Count spikes within bin indices
+    spikes_presentation(i,idx) = spkCounts; % Mark spike presentations
     
     % Convoluting the spike times with a n points gaussian convolution
     spikes_presentation(i,:) = nanconv(spikes_presentation(i,:),gausswin(convolution_points)'/sum(gausswin(convolution_points)),'edge');
---
+++ utilities/concatenateCells.m
@@ -0,0 +1,39 @@
+function concatenatedMatrix = concatenateCells(cellArray, dimension)
+% concatenatedMatrix = concatenateCells(cellArray, dimension)
+%
+% Concatenates elements of a one-dimensional cell array into a matrix and
+% outputs it.
+%
+% Args:
+%   cellArray (cell): a shape-(M, N) cell array where M = 1 or N = 1. The
+%     elements are expected to be numeric.
+%   dimension (numeric, optional): a shape-(1, 1) numeric type variable
+%     indicating the dimension along which to concatenate. 1 is default.
+%
+% Returns:
+%   concatenatedMatrix (numeric): a shape-(M, N) numeric matrix containing
+%     concatentated elements of the input cell array.
+
+arguments
+  cellArray cell
+  dimension (1,1) {mustBeNumeric} = 1
+end
+
+% Concatenate
+nElements = numel(cellArray);
+concatenatedMatrix = cellArray{1};
+assert(isnumeric(concatenatedMatrix), 'Elements of the supplied cell array must be numeric.');
+for iElement = 2:nElements
+  assert(isnumeric(cellArray{iElement}), 'Elements of the supplied cell array must be numeric.');
+  if dimension == 1
+    assert(size(concatenatedMatrix,2) == size(cellArray{iElement},2), ...
+      'Elements in the supplied cell array have unequal number of columns');
+    concatenatedMatrix = [concatenatedMatrix; cellArray{iElement}]; %#ok<*AGROW> 
+  elseif dimension == 2
+    assert(size(concatenatedMatrix,1) == size(cellArray{iElement},1), ...
+      'Elements in the supplied cell array have unequal number of rows');
+    concatenatedMatrix = [concatenatedMatrix, cellArray{iElement}];
+  end
+end
+
+assert(isnumeric(concatenatedMatrix));
