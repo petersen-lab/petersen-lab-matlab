0a1ec7132ddc96851f2f3ee251a8f40dfa8f0dc5
dervinism <martynas.dervinis@gmail.com>
2023-11-20 01:21:33 +0100

    Regular commit 20/11/2023

--- spikes/saveOscScore.m
+++ spikes/saveOscScore.m
@@ -12,6 +12,15 @@ function saveOscScore(spikesFile, options)
 %     score (default=[4 11]);
 %   sr (numeric, optional, keyword): a shape-(1, 1) numeric scalar
 %     frequency for sampling the signal in Hz (default=500).
+%   shuffle (logical | struct, optional, keyword): a shape-(1, 1) logical
+%     scalar to indicate whether the 95% significance level cut-off should
+%     be calculated based on randomly shuffling the data (default=false).
+%     Alternatively, one can supply a shape-(1, 1) structure scalar
+%     specifying the parameters of the random number generator to be used
+%     for data shuffling (guarantees reproducibility). The structure should
+%     have fields 'Type' and 'Seed' which specify the type of the random
+%     number generator and the numeric seed. For more information, check
+%     the documentation of the Matlab's rng function.
 %   outputFilename (char, optional, keyword): a shape-(1, N) character
 %     array with a full path filename where the unit oscillation scores
 %     should be saved. The filename should end with
@@ -34,6 +43,11 @@ function saveOscScore(spikesFile, options)
 % Returns:
 %   None.
 %
+% Comments:
+%   If shuffle parameter is enabled, the shuffled data will be saved in a
+%   separate file with the 'oscillationScore' part replaced by
+%   'oscillationScoreShuffled'.
+%
 % Authors:
 %   Martynas Dervinis (martynas.dervinis@gmail.com).
 
@@ -41,6 +55,7 @@ arguments
   spikesFile (1,:) {mustBeA(spikesFile,'char')}
   options.freqRange (1,2) {mustBePositive,mustBeVector} = [4 11]
   options.sr (1,1) {mustBePositive} = 500
+  options.shuffle (1,1) {mustBeLogicalOrListedType(options.shuffle,'struct')} = false;
   options.outputFilename (1,:) {mustBeA(options.outputFilename,'char')} = ''
 end
 
@@ -54,8 +69,8 @@ end
 load(spikesFile); %#ok<*LOAD>
 
 % Detect oscillation scores and frequencies of individual units
-[oscScore, oscFreq] = multiOscScore(spikes.times, freqRange=options.freqRange, ...
-  sr=options.sr);
+[oscScore, oscFreq, ~, shuffledOscScore] = multiOscScore(spikes.times, freqRange=options.freqRange, ...
+  sr=options.sr, shuffle=options.shuffle);
 
 % Organise oscillation score data within the cellinfo container
 oscillationScore.data = [oscScore, oscFreq];
@@ -69,6 +84,14 @@ oscillationScore.processingInfo.function = 'petersen-lab-matlab/spikes/multiOscS
 oscillationScore.processingInfo.date = datetime;
 oscillationScore.processingInfo.username = getenv('username');
 oscillationScore.processingInfo.hostname = getenv('computername');
+
+oscillationScoreShuffled = oscillationScore;
+oscillationScoreShuffled.data = shuffledOscScore;
+oscillationScoreShuffled.definition = ['Shuffled unit oscillation scores ' ...
+  'calculated as in Muresan et al. (2008) ' ...
+  'The Oscillation Score: An Efficient Method for Estimating Oscillation ' ...
+  'Strength in Neuronal Activity , Journal of Neurophysiology 99: 1333-1353 ' ...
+  '(https://www.raulmuresan.ro/sources/oscore/).'];
 
 % Save unit oscillation scores
 if isempty(options.outputFilename)
@@ -89,3 +112,7 @@ elseif ~contains(options.outputFilename, 'oscillationScore.cellinfo.mat', ...
   end
 end
 save(options.outputFilename, 'oscillationScore', '-v7.3');
+
+options.outputFilename = strrep(options.outputFilename, ...
+  'oscillationScore', 'oscillationScoreShuffled');
+save(options.outputFilename, 'oscillationScoreShuffled', '-v7.3');
