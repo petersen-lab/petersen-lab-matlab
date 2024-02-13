3cd40c9a126c380146257e990a05f1afc32d2239
dervinism <martynas.dervinis@gmail.com>
2024-02-13 13:37:09 +0100

    Added more fit types to fitPlotEllipse

--- utilities/fitPlotEllipse.m
+++ utilities/fitPlotEllipse.m
@@ -10,21 +10,39 @@ function [equationCoeffs, parameters, RSquare, ellipse] = fitPlotEllipse(xy, opt
 %     the y coordinates.
 %   type (char, optional, keyword): a shape-(1, M) character array
 %     describing fit type. Two fit types are available:
-%     'ellipseHyperbola' - uses EllipseFitByTaubin function to fit the data
-%                          (default). It returns an ellipse or a hyperbola,
+%     'ellipseLinear' - uses fitellipse function with the linear least
+%                       squares method and 'bookstein' property to fit the
+%                       data.
+%     'ellipseLinearTrace' - uses fitellipse function with the linear least
+%                            squares method and 'trace' property to fit the
+%                            data.
+%     'ellipseNonlinear' - uses fitellipse function with the nonlinear
+%                          least squares method to fit the data (default).
+%     'ellipseHyperbola' - uses EllipseFitByTaubin function to fit the
+%                          data. It returns an ellipse or a hyperbola,
 %                          if the latter approximates the data better.
 %     'ellipseOnly' - uses EllipseDirectFit to fit the data and always
 %                     returns an ellipse.
 %   draw (logical, optional, keyword): a shape-(1, 1) logical scalar
 %     indicating whether to draw the fitted ellipse (default=false).
+%   maxits (numeric, optional, keyword): a shape-(1, 1) numeric scalar that
+%     is a positive integer defining the maximum number of iterations for
+%     the Gauss Newton step in the case when 'ellipseNonlinear' fit type is
+%     used (default=200).
+%   tol (numeric, optional, keyword): a shape-(1, 1) numeric scalar that
+%     is a positive real number defining the relative step size tolerance
+%     when 'ellipseNonlinear' fit type is used (default=1e-5).
 %
 % Returns:
 %   equationCoeffs (numeric): a shape-(6, 1) numeric array of algebraic
 %     coefficients of the fitted ellipse equation in the form
-%     [a; b; c; d; e; f], so that ax^2 + bxy + cy^2 +dx + ey + f = 0.
+%     [a; b; c; d; e; f], so that ax^2 + bxy + cy^2 +dx + ey + f = 0. In
+%     cases where 'ellipseLinear', 'ellipseLinearTrace', or
+%     'ellipseNonlinear' fitting type options are used, this output
+%     variable is empty.
 %   parameters (struct): a shape-(1, 1) structure scalar with the geometric
-%     ellipse parameters with fields radii, U, and x0, where y = U'*(x-x0),
-%     z = y./radii, and |z|^2 = sum(z.^2) = 1.
+%     ellipse parameters with fields radii, U (rotation matrix), and x0
+%     (centre), where y = U'*(x-x0), z = y./radii, and |z|^2 = sum(z.^2) = 1.
 %   RSquare (numeric): a shape-(1, 1) numeric scalar providing an estimate
 %     of explained variance by the fit (R-square).
 %   ellipse (numeric): a shape-(2, 181) numeric array of x and y
@@ -38,6 +56,8 @@ function [equationCoeffs, parameters, RSquare, ellipse] = fitPlotEllipse(xy, opt
 % Dependencies:
 %   petersen-lab/petersen-lab-matlab
 %     (https://github.com/petersen-lab/petersen-lab-matlab/).
+%   fitellipse
+%     (https://se.mathworks.com/matlabcentral/fileexchange/15125-fitellipse-m?s_tid=srchtitle).
 %   EllipseFitByTaubin
 %     (https://se.mathworks.com/matlabcentral/fileexchange/22683-ellipse-fit-taubin-method).
 %   EllipseDirectFit
@@ -50,34 +70,55 @@ function [equationCoeffs, parameters, RSquare, ellipse] = fitPlotEllipse(xy, opt
 
 arguments
   xy (:,2) {mustBeNumeric}
-  options.type (1,:) {mustBeMember(options.type,{'ellipseHyperbola','ellipseOnly'})} = 'ellipseHyperbola'
+  options.type (1,:) {mustBeMember(options.type,{'ellipseLinear','ellipseLinearTrace','ellipseNonlinear','ellipseHyperbola','ellipseOnly'})} = 'ellipseNonlinear'
   options.draw (1,1) {logical} = false
+  options.maxits (1,1) {mustBePositive} = 200
+  options.tol (1,1) {mustBeReal} = 1e-5
 end
 
 % Fit the ellipse to data
-if strcmpi(options.type, 'ellipseHyperbola')
+if strcmpi(options.type, 'ellipseLinear')
+  [z, a, b, alpha] = fitellipse(xy', 'linear');
+elseif strcmpi(options.type, 'ellipseLinearTrace')
+  [z, a, b, alpha] = fitellipse(xy', 'linear', 'constraint','trace');
+elseif strcmpi(options.type, 'ellipseNonlinear')
+  [z, a, b, alpha] = fitellipse(xy', 'maxits',options.maxits, 'tol',options.tol);
+elseif strcmpi(options.type, 'ellipseHyperbola')
   equationCoeffs = EllipseFitByTaubin(xy);
 elseif strcmpi(options.type, 'ellipseOnly')
   equationCoeffs = EllipseDirectFit(xy);
 end
 
 % Convert the equation coefficients to the polar (parametric) repsentation
-A = equationCoeffs(1);
-B = equationCoeffs(2);
-C = equationCoeffs(3);
-D = equationCoeffs(4);
-E = equationCoeffs(5);
-F = equationCoeffs(6);
-H = [A,   B/2;
-     B/2, D];
-g = 0.5*[C; E];
-c = F;
-[parameters.radii, parameters.U, parameters.x0] = EllAlg2Geo(H, g, c);
+if strcmpi(options.type, 'ellipseHyperbola') || strcmpi(options.type, 'ellipseOnly')
+  A = equationCoeffs(1);
+  B = equationCoeffs(2);
+  C = equationCoeffs(3);
+  D = equationCoeffs(4);
+  E = equationCoeffs(5);
+  F = equationCoeffs(6);
+  H = [A,   B/2;
+    B/2, D];
+  g = 0.5*[C; E];
+  c = F;
+  [parameters.radii, parameters.U, parameters.x0] = EllAlg2Geo(H, g, c);
+else
+  equationCoeffs = [];
+  parameters.radii = [a; b];
+  parameters.U = [cos(alpha), -sin(alpha); sin(alpha) cos(alpha)];
+  parameters.x0 = z;
+end
 
-% Points on ellipse, parametric 
-Ea = parameters.U*diag(parameters.radii);
-theta = linspace(0,2*pi,181);
-ellipse = parameters.x0 + Ea*[cos(theta); sin(theta)];
+% Points on ellipse, parametric
+npts = 181;
+theta = linspace(0,2*pi,npts);
+if strcmpi(options.type, 'ellipseHyperbola') || strcmpi(options.type, 'ellipseOnly')
+  Ea = parameters.U*diag(parameters.radii);
+  ellipse = parameters.x0 + Ea*[cos(theta); sin(theta)];
+else
+  Q = [cos(alpha), -sin(alpha); sin(alpha) cos(alpha)];
+  ellipse = Q * [a*cos(theta); b*sin(theta)] + repmat(z, 1, npts);
+end
 
 % Implicit function on grid
 %[minxy, maxxy] = bounds(ellipse,2);
